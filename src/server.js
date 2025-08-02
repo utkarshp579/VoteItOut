b4d3a7df67e86806e9fb35b15d017ae1ea731899
Utkarsh13124 <utkarshp579@gmail.com>
2025-08-02 15:33:20 +0530

    Socket.io recieving connections set, server side works completed

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ export default function startServer(store) {
 
     store.subscribe(() => io.emit("state", store.getState().toJS()));
     io.on("connection", (socket) => {
-      socket.emit("state", store.getState().toJS());
+        socket.emit("state", store.getState().toJS());
+        socket.on("action", store.dispatch.bind(store));
     });
 }
